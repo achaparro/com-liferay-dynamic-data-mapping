fab606e77e2193b4e348a0cfa81e06318578a7d4
daviddotzhang <david.zhang@liferay.com>
2017-06-30 09:42:15 +0800

    LPS-73385 - use _AlterColumnType

--- dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_4/UpgradeDDMTemplateSmallImageURL.java
+++ dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_4/UpgradeDDMTemplateSmallImageURL.java
@@ -26,7 +26,7 @@ public class UpgradeDDMTemplateSmallImageURL extends UpgradeProcess {
 	protected void doUpgrade() throws Exception {
 		alter(
 			DDMTemplateTable.class,
-			new AlterColumnName("smallImageURL", "smallImageURL String null"));
+			new AlterColumnType("smallImageURL", "smallImageURL String null"));
 	}
 
 }
